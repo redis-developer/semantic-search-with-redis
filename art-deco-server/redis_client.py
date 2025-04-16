12d5c6779b9e00e5fe14819deb5aafee2969c745
Guy Royse <guy@guyroyse.com>
2025-04-16 16:19:03 -0400

    rest api seems solid

--- art-deco-server/redis_client.py
+++ art-deco-server/redis_client.py
@@ -1,6 +1,35 @@
 import redis
+from redis import ResponseError
+from redis.commands.search.field import TextField, VectorField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 
 
 db = redis.Redis(host="localhost", port=6379, decode_responses=False)
 
-# TODO: create search index here
+# the name of the index
+index_name = "idx:items"
+
+# the index definition
+prefixes = ["item:"]
+index_type = IndexType.HASH
+definition = IndexDefinition(prefix=prefixes, index_type=index_type)
+
+# the schema for the index
+schema = (
+    TextField("title"),
+    TextField("author"),
+    TextField("description"),
+    VectorField(
+        "embedding",
+        "FLAT",
+        {"TYPE": "FLOAT32", "DIM": 512, "DISTANCE_METRIC": "COSINE"},
+    ),
+)
+
+
+try:
+    db.ft(index_name).info()
+except ResponseError as err:
+    if "no such index" not in str(err):
+        raise err
+    db.ft(index_name).create_index(fields=schema, definition=definition)
