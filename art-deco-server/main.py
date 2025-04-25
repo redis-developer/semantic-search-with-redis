6a2095967bb9a2cba73063d428abeab4ef9d59dd
Guy Royse <guy@guyroyse.com>
2025-04-25 12:59:13 -0400

    added very permission CORS settings

--- art-deco-server/main.py
+++ art-deco-server/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.item_routes import router as item_router
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(item_router, prefix="/items")
 
 
